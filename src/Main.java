bacc8c280a216a9a28c5507c0e288739895a6cab
Shravan <shravan097@users.noreply.github.com>
2017-12-21 14:19:10 -0500

    Trying Get Available Train

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JFrame;
@@ -6,13 +7,19 @@ public class Main {
 
     public static void main(String[] args) {
 
+		TrainQuery available_train = new TrainQuery();
+		List<Train>  result = available_train.getAllTrain();
+
+		for( Train i: result)
+			System.out.printf("Train_ID: %d\tStart_Station: %s\tEnd_Station: %s\tTime_Of_Day: %s\n",
+				 i.getTrain_id(),i.getStart_station(),i.getEnd_station(),i.getTime_of_day());
 
         // TESTING
         
-    			testConnection test = new testConnection();
-    		     GUI tp = new GUI();
-    		        tp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    		        tp.setVisible(true);
+//    			testConnection test = new testConnection();
+//    		     GUI tp = new GUI();
+//    		        tp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//    		        tp.setVisible(true);
     }
        
 }
